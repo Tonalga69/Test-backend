8c815bd0915f26d60892a1495500bd17c357c982
SamuelR69 <a22310392@ceti.mx>
2023-04-21 14:41:04 -0600

    Update createTask.js

--- src/tasks/controllers/createTask.js
+++ src/tasks/controllers/createTask.js
@@ -1,27 +1,27 @@
 const { request, response } = require("express");
 
-const {PrismaClient} = require("@prisma/client"); 
-const prisma= new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
+const createTask = async (req = request, res = response) => {
+  //in this case i won´t allow creating comments when creating a task
+  const { body } = req;
+  delete body.comments;
 
-
-const createTask= async(req= request, res= response)=>{
-    //in this case i won´t allow creating comments when creating a task
-    const {body} = req; 
-    delete body.comments; 
-
-    const task= await prisma.task.create({
-        data:{
-            ...body
-        }
-    }).catch(error=> console.error(error)); 
-
-    if(!task) return res.status(400).json({message: "Bad request"}); 
-
-    return res.status(202).json({
-        message: "created", 
-        data: task
+  const task = await prisma.task
+    .create({
+      data: {
+        ...body,
+      },
     })
-}
+    .catch((error) => console.error(error));
 
-module.exports=createTask; 
+  if (!task) return res.status(400).json({ message: "Bad request" });
+
+  return res.status(202).json({
+    message: "created",
+    data: task,
+  });
+};
+
+module.exports = createTask;
